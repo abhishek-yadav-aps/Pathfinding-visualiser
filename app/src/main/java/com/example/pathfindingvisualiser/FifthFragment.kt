0ce0a9c463a27c7f8837f787e51cb2beac78a967
Adarsh Singh <2001adarshsingh@gmail.com>
2020-05-10 11:08:50 +0530

    Videos added

--- app/src/main/java/com/example/pathfindingvisualiser/FifthFragment.kt
+++ app/src/main/java/com/example/pathfindingvisualiser/FifthFragment.kt
@@ -1,10 +1,17 @@
 package com.example.pathfindingvisualiser
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import kotlinx.android.synthetic.main.fragment_fifth.*
+import kotlinx.android.synthetic.main.fragment_fourth.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass.
@@ -18,5 +25,18 @@ class FifthFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fifth, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            var mediaController = MediaController(activity)
+            videoView2.setMediaController(mediaController)
+            val path = "android.resource://com.example.pathfindingvisualiser/"+ R.raw.dijs;
+            val uri = Uri.parse(path)
+            videoView2.setVideoURI(uri)
+            videoView2.start()
+        }
+}
 
 }
--- app/src/main/java/com/example/pathfindingvisualiser/FourthFragment.kt
+++ app/src/main/java/com/example/pathfindingvisualiser/FourthFragment.kt
@@ -1,10 +1,16 @@
 package com.example.pathfindingvisualiser
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import kotlinx.android.synthetic.main.fragment_fourth.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +23,21 @@ class FourthFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_fourth, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            var mediaController = MediaController(activity)
+            videoView.setMediaController(mediaController)
+            val path = "android.resource://com.example.pathfindingvisualiser/"+ R.raw.maze;
+            val uri = Uri.parse(path)
+            videoView.setVideoURI(uri)
+            videoView.start()
+
+        }
+
     }
 
 }
